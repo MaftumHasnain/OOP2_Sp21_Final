f48d3446067cd86fdb287d4f2ba479f51d2bc3a8
MaftumHasnain <maftumhasnain@gmail.com>
2021-03-22 15:25:19 +0600

    lab 5

---
+++ AccountTransaction/AccountTransaction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTransaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Account a1 = new Account("Maftum", "8989", 200);
+            Account a2 = new Account("Hasnain", "2323", 300);
+            a1.Withdraw(50);
+            a1.Deposit(200);
+            a1.Transfer(300, a2);
+            a1.showAllTransactions();
+            a2.showAllTransactions();
+        }
+    }
+}
---
+++ AccountTransaction/AccountTransaction/Transaction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTransaction
+{
+    class Transaction
+    {
+        public Account Sender { get; set; }
+        public Account Receiver { get; set; }
+        public int Ammount { get; set; }
+        public String AdditionalInfo { get; set; }
+
+        public Transaction() { }
+        public Transaction(Account Sender, Account Receiver, int Ammount, String AdditionalInfo)
+        {
+            this.Sender = Sender;
+            this.Receiver = Receiver;
+            this.Ammount = Ammount;
+            this.AdditionalInfo = AdditionalInfo;
+        }
+
+        public void showInfo()
+        {
+            Console.WriteLine("Sender Name:{0} Account ID:{1}", Sender.AccName, Sender.Accid);
+            Console.WriteLine("Receiver Name:{0} Account ID:{1}", Receiver.AccName, Receiver.Accid);
+            Console.WriteLine("Ammount:" + Ammount);
+            Console.WriteLine("Additional Information:" + AdditionalInfo);
+            Console.WriteLine();
+        }
+    }
+}
